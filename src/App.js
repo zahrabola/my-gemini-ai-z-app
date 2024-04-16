c5e34fd6c01f87d9d87c332823e48a0a500c279b
zahrabola <95353397+zahrabola@users.noreply.github.com>
2024-04-16 15:31:09 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Sidebar from './Components/Sidebar';
 
 function App() {
   return (
     <div className="App">
      app
+     <Sidebar />
     </div>
   );
 }
--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -11,12 +11,26 @@ const Sidebar = () => {
           <p>New Chat</p>
         </div>
         <div className="recent">
-          <p className="recenttitle">New Chat</p>
+          <p className="recenttitle">Recent</p>
           <div className="recententry">
             <img src={assets.menuicon} alt="menu icon" className="menu-icon" />
+            <p>What is React</p>
           </div>
         </div>
-        <div className="botttom"></div>
+      </div>
+      <div className="botttom">
+        <div className="bottomitem recententry">
+        <img src={assets.questionicon} alt="question icon" className="question-icon" />
+        <p>Help</p>
+        </div>
+        <div className="bottomitem recententry">
+        <img src={assets.historyicon} alt="history icon" className="history-icon" />
+        <p>Activity</p>
+        </div>
+        <div className="bottomitem recententry">
+        <img src={assets.settingicon} alt="setting icon" className="setting-icon" />
+        <p>Setting</p>
+        </div>
       </div>
     </div>
   );
