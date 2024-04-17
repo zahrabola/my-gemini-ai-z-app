08e9e2b0b416e9d622b23c1fd4fc91fdc6249912
zahrabola <95353397+zahrabola@users.noreply.github.com>
2024-04-17 14:56:50 +0100

    Update Css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
+///import logo from './logo.svg';
 import './App.css';
 import Sidebar from './Components/Sidebar';
+import Main from './Components/Main';
 
 function App() {
   return (
     <div className="App">
-     
-     <Sidebar />
+<Sidebar /> 
+     <Main /> 
     </div>
   );
 }
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import { assets } from "../Assets/Data/assetsimg";
 
 const Main = () => {
     return (
-        <div className='main-container'>
-            
+        <div className='main'>
+            <div className='nav'>
+                <p>Gemini </p>
+                <img src={assets.usericon} alt="usericon" className="user-icon" />
+            </div>
+            <div className='main-container'>
+                <div className='greeting'>
+                    <p><span>Hello Zahra.</span></p>
+                    <p>How can I help you today?</p>
+                </div>
+                <div className='cards'>
+                    <div className='card'>
+                        <p>Suggest beautiful places to see on an upcoming road trip</p>
+                        <img src={assets.compassicon} alt='compass icon' className='compass-icon' />
+                    </div>
+                    <div className='card'>
+                        <p>Brainstorm road trip ideas</p>
+                        <img src={assets.bulbicon} alt='bulbicon' className='bulb-icon' />
+                    </div>
+                    <div className='card'>
+                        <p>Suggest beautiful places to see on an upcoming road trip</p>
+                        <img src={assets.messageicon} alt='message icon' className='message-icon' />
+                    </div>
+                    <div className='card'>
+                        <p>Suggest beautiful places to see on an upcoming road trip</p>
+                        <img src={assets.codeicon} alt='code icon' className='code-icon' />
+                    </div>
+
+                </div>
+            </div>
         </div>
     );
 }
