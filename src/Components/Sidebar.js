1e457f6c19d2c4c97f5b1e072b6660cfe8fa6bd0
zahrabola <95353397+zahrabola@users.noreply.github.com>
2024-04-17 14:05:28 +0100

    Update Sidebar
    useState - ternary operator

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,35 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import { assets } from "../Assets/Data/assetsimg";
 
 const Sidebar = () => {
+  const [isopen, setIsOpen] = useState(false);
+
   return (
     <div className="sidebar-container">
       <div className="top">
         <img src={assets.menuicon} alt="menu icon" className="menu-icon" />
         <div className="newchat">
           <img src={assets.plusicon} alt="plus icon" className="plus-icon" />
-          <p>New Chat</p>
+          {isopen ? <p>New Chat</p> : null}
         </div>
-        <div className="recent">
-          <p className="recenttitle">Recent</p>
-          <div className="recententry">
-            <img src={assets.messageicon} alt="message icon" className="message-icon" />
-            <p>What is React....</p>
+        {isopen ? (
+          <div className="recent">
+            <p className="recenttitle">Recent</p>
+            <div className="recententry">
+              <img
+                src={assets.messageicon}
+                alt="message icon"
+                className="message-icon"
+              />
+              <p>What is React....</p>
+            </div>
           </div>
-        </div>
+        ) : null}
       </div>
       <div className="botttom">
         <div className="bottomitem recententry">
-        <img src={assets.questionicon} alt="question icon" className="question-icon" />
-        <p>Help</p>
+          <img
+            src={assets.questionicon}
+            alt="question icon"
+            className="question-icon"
+          />
+          {isopen ? <p>Help</p> : null }
         </div>
         <div className="bottomitem recententry">
-        <img src={assets.historyicon} alt="history icon" className="history-icon" />
-        <p>Activity</p>
+          <img
+            src={assets.historyicon}
+            alt="history icon"
+            className="history-icon"
+          />
+         {isopen ? <p>Activity</p> : null} 
         </div>
         <div className="bottomitem recententry">
-        <img src={assets.settingicon} alt="setting icon" className="setting-icon" />
-        <p>Setting</p>
+          <img
+            src={assets.settingicon}
+            alt="setting icon"
+            className="setting-icon"
+          />
+          {isopen ? <p>Setting</p> : null}
         </div>
       </div>
     </div>
